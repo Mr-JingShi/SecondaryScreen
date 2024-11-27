af8dc558e5a14af0b3c260bf950819e58707a687
chengjian.scj <chengjian.scj@alibaba-inc.com>
2024-11-27 21:17:28 +0800

    chore 兼容Android 15

--- server/src/main/java/com/secondaryscreen/server/KillSelf.java
+++ server/src/main/java/com/secondaryscreen/server/KillSelf.java
@@ -1,11 +1,12 @@
 package com.secondaryscreen.server;
 
+import android.content.Intent;
 import android.content.pm.IOnAppsChangedListener;
 import android.content.pm.ParceledListSlice;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IRemoteCallback;
-import android.os.RemoteException;
 import android.os.UserHandle;
 
 public class KillSelf {
@@ -15,8 +16,34 @@ public class KillSelf {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             ServiceManager.getPackageManager().registerPackageMonitorCallback(new IRemoteCallback.Stub() {
                 @Override
-                public void sendResult(Bundle data) {
-                    Ln.i(TAG, "sendResult data:" + data);
+                public void sendResult(Bundle bundle) {
+                    Ln.i(TAG, "sendResult bundle:" + bundle);
+                    Intent intent = bundle.getParcelable(
+                            "android.content.pm.extra.EXTRA_PACKAGE_MONITOR_CALLBACK_RESULT", Intent.class);
+                    if (intent != null) {
+                        String action = intent.getAction();
+                        Uri uri = intent.getData();
+                        String packageName = uri != null ? uri.getSchemeSpecificPart() : null;
+                        if (Intent.ACTION_PACKAGE_ADDED.equals(action)) {
+                            Ln.i(TAG, "onPackageAdded:" + packageName);
+                        } else if (Intent.ACTION_PACKAGE_REMOVED.equals(action)) {
+                            Ln.i(TAG, "onPackageRemoved:" + packageName);
+
+                            KillSelf.kill(packageName);
+                        } else if (Intent.ACTION_PACKAGE_CHANGED.equals(action)) {
+                            Ln.i(TAG, "onPackageChanged:" + packageName);
+
+                            KillSelf.kill(packageName);
+                        } else if (Intent.ACTION_PACKAGE_DATA_CLEARED.equals(action)) {
+                            Ln.i(TAG, "onPackageDataCleared:" + packageName);
+                        } else if (Intent.ACTION_PACKAGE_RESTARTED.equals(action)) {
+                            Ln.i(TAG, "onPackageRestarted:" + packageName);
+                        } else if (Intent.ACTION_PACKAGES_SUSPENDED.equals(action)) {
+                            Ln.i(TAG, "onPackagesSuspended:" + packageName);
+                        } else if (Intent.ACTION_PACKAGES_UNSUSPENDED.equals(action)) {
+                            Ln.i(TAG, "onPackagesUnsuspended:" + packageName);
+                        }
+                    }
                 }
             }, 0);
         } else {
@@ -37,7 +64,7 @@ public class KillSelf {
                 public void onPackageChanged(UserHandle user, String packageName) {
                     Ln.i(TAG, "onPackageChanged:" + packageName);
 
-                    KillSelf.kill(packageName);
+                    // KillSelf.kill(packageName);
                 }
 
                 @Override
